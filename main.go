cbfce105734d615fbacca266dd05ebd2cf33de82
Md. Pieash Ahmed <pieash9@gmail.com>
2025-06-24 22:38:06 +0600

    refactor: streamline main function and encapsulate operation processing

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import "fmt"
 
+func processOperation(a int, b int, op func(p int, q int)) {
+	op(a, b)
+}
+
+func add(a int, b int) {
+	c := a + b
+	fmt.Println(c)
+}
+
 func main() {
-
-	subtraction := func(a, b int) {
-		fmt.Println(a - b)
-	}
-	subtraction(66, 45) // will work
-
-	add(2, 3) // not work but in global it will work
-
-	// function expression
-	add := func(a, b int) {
-		fmt.Println(a + b)
-	}
+	processOperation(6, 8, add)
 }
