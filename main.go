b18fa631fdeb50210f8bc148377a1f6bc809b556
Md. Pieash Ahmed <pieash9@gmail.com>
2025-06-26 18:14:30 +0600

    init

--- main.go
+++ main.go
@@ -2,23 +2,36 @@ package main
 
 import "fmt"
 
-var a = 100 // data segment
+const a = 10 // constant
+var p = 100
 
-func call() { // code segment
-	add := func(x, y int) {
-		z := x + y
-		fmt.Println(z)
+func outer() func() {
+	money := 100
+	age := 30
+	fmt.Println("Age = ", age)
+
+	show := func() {
+		money = money + a + p
+		fmt.Println(money)
 	}
-	add(5, 6)
-	add(51, 61)
 
+	return show
 }
 
-func main() { // code segment
+func call() {
+	incr1 := outer()
+	incr1()
+	incr1()
+
+	incr2 := outer()
+	incr2()
+	incr2()
+}
+
+func init() {
+	fmt.Println("====Bank====")
+}
+
+func main() {
 	call()
-	fmt.Println(a)
-}
-
-func init() { // code segment
-	fmt.Println("Hello")
 }
