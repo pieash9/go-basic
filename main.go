f47b73306f57e8ac90135d6a4fc33e8191143f76
Md. Pieash Ahmed <pieash9@gmail.com>
2025-06-26 17:52:32 +0600

    style: add comments to clarify code segments

--- main.go
+++ main.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-var a = 100
+var a = 100 // data segment
 
-func call() {
+func call() { // code segment
 	add := func(x, y int) {
 		z := x + y
 		fmt.Println(z)
-
 	}
 	add(5, 6)
 	add(51, 61)
 
 }
-func main() {
+
+func main() { // code segment
 	call()
 	fmt.Println(a)
 }
 
-func init() {
+func init() { // code segment
 	fmt.Println("Hello")
 }
