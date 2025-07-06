0243b4f85adf6c17aeb7fdbdf114d329f9f33787
Md. Pieash Ahmed <pieash9@gmail.com>
2025-07-06 16:25:04 +0600

    refactor: reorganize array initialization and printing in Arrays function

--- 2_arrays/arrays.go
+++ 2_arrays/arrays.go
@@ -2,10 +2,21 @@ package arrays
 
 import "fmt"
 
+var (
+	arr2 = [3]string{"I", "am", "Rahim"}
+)
+
 func Arrays() {
 	var arr [2]int
-
+	arr[0] = 5
+	arr[1] = 10
 	fmt.Println(arr)
+
+	arr1 := [2]int{3, 6}
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
+	fmt.Println(arr2[2])
 }
 
 /*
