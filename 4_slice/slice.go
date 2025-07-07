64448733f046ee529e795884a9860d21958312c6
Md. Pieash Ahmed <pieash9@gmail.com>
2025-07-07 07:45:17 +0600

    refactor: enhance Slice function with additional comments and formatting

--- 4_slice/slice.go
+++ 4_slice/slice.go
@@ -1,5 +1,20 @@
 package slice
 
-func Slice() {
+import "fmt"
 
+func Slice() {
+	arr := [6]string{"this", "is", "a", "go", "interview", "Ques"}
+	fmt.Println(arr)
 }
+
+/*
+	2. Phases:
+		1. compilation phase (compile time)
+		2. execution phase (run time)
+
+  *************** compile phase ************
+  ** code segment **
+  print = func(numbers int[3]) {...}
+	main = func () {...}
+
+*/
