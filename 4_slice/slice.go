f9631e02bea7917ba64a7396307601db9af902a6
Md. Pieash Ahmed <pieash9@gmail.com>
2025-07-08 17:03:12 +0600

    feat: update Slice function to demonstrate slice operations with detailed comments

--- 4_slice/slice.go
+++ 4_slice/slice.go
@@ -3,16 +3,19 @@ package slice
 import "fmt"
 
 func Slice() {
-	arr := [6]string{"this", "is", "a", "go", "interview", "Ques"}
-	fmt.Println(arr)
+	var x []int      // [], ptr = nil, len = 0, cap = 0
+	x = append(x, 1) // [1], ptr = 26 ,len = 1, cap = 1 (heap e rakhbe value)
+	x = append(x, 2) // [1, 2], ptr = 27 ,len = 2, cap = 2 (heap e rakhbe value)  // capacity & length same hole 2 dara gun krbe
+	x = append(x, 3) // [1, 2, 3], ptr = 29 ,len = 3, cap = 3 (heap e rakhbe value)  // capacity & length same hole 2 dara gun krbe
+	y := x
 
-	s := arr[1:4]
-	fmt.Println(s) // ["is" "a" "go"]
+	x = append(x, 4)
+	y = append(y, 5)
 
-	s1 := s[1:2]
-	fmt.Println(s1) // ["a"], len = 1, cap = 4
-	fmt.Println(len(s1))
-	fmt.Println(cap(s1))
+	x[0] = 10
+
+	fmt.Println(x) // [10 2 3 5]
+	fmt.Println(y) // [10 2 3 5]
 }
 
 /*
@@ -26,10 +29,11 @@ func Slice() {
 	main = func () {...}
 
 	{
-**	slice => Ptr=1, len=3, cap=5
-** Array => arr := [6]string{"this", "this", "is", "a", "go", "interview", "Ques"}
+	**	slice => Ptr=1, len=3, cap=5
+	** Array => arr := [6]string{"this", "this", "is", "a", "go", "interview", "Ques"}
 	so here arr[1:4] => ptr=1, len=3(je koita value ache), cap=5(jekhane thke shuru hoise ekbre shesh prjnto)
 
 	}
 
+	7. slice underlying array rule => 1024 * 2 = 2048 -> double kore parbe capacity.. but 1024 par hoye gele 25% kore barbe.
 */
